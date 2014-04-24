653cb952a38fb7a275c8173c8630a52b72d4d9ff
StCredZero <stcredzero@gmail.com>
2014-04-24 16:04:35 -0700

    game loop

--- main.go
+++ main.go
@@ -1,50 +1,133 @@
 package main
 
 import (
-	//"bytes"
 	"flag"
 	"fmt"
 	"github.com/gorilla/websocket"
 	"go/build"
 	"log"
+	"math/rand"
 	"net/http"
 	"path/filepath"
+	"runtime"
 	"text/template"
+	"time"
 )
 
-type cstserver struct {
-	// Registered connections.
-	connections map[*connection]bool
+const subgrid_width = 79
+const subgrid_height = 25
 
-	// Inbound messages from the connections.
-	broadcast chan []byte
+type Coord struct {
+	x int64
+	y int64
+}
+
+func randomSubgridCoord() Coord {
+	return Coord{
+		x: int64(rand.Intn(subgrid_width)),
+		y: int64(rand.Intn(subgrid_height)),
+	}
+}
+
+type Entity struct {
+	Id       uint32
+	Location Coord
+	//MoveQueue [1024]chan rune
+}
+
+type SubGrid struct {
+	GridCoord Coord
+	Grid      map[Coord]uint32
+	Entities  map[uint32]Entity
+}
+
+func (self *SubGrid) EmptyAt(loc Coord) bool {
+	_, present := self.Grid[loc]
+	return !present
+}
+
+func (self *SubGrid) NewEntity(id uint32) Entity {
+	var loc = randomSubgridCoord()
+	for !self.EmptyAt(loc) {
+		loc = randomSubgridCoord()
+	}
+	self.Entities[id] = Entity{
+		Id:       id,
+		Location: loc,
+	}
+	self.Grid[loc] = id
+	return self.Entities[id]
+}
+
+type WorldGrid struct {
+	grid       map[Coord]SubGrid
+	entityGrid map[uint32]Coord
+}
+
+func IdGenerator(lastId uint32) chan (uint32) {
+	next := make(chan uint32)
+	id := lastId + 1
+	go func() {
+		for {
+			next <- id
+			id++
+		}
+	}()
+	return next
+}
+
+type CstServer struct {
+	// Registered connections.
+	connections map[*connection]uint32
 
 	// Register requests from the connections.
 	register chan *connection
 
 	// Unregister requests from connections.
 	unregister chan *connection
+
+	entityIdGen chan uint32
+
+	world SubGrid
 }
 
-func (srv *cstserver) run() {
+func (srv *CstServer) run() {
+	timer := time.Tick(125 * time.Millisecond)
 	for {
+		runtime.Gosched()
 		select {
 		case c := <-srv.register:
-			srv.connections[c] = true
+			println("starting register")
+			var newId = <-srv.entityIdGen
+			srv.connections[c] = newId
+			var newEntity = srv.world.NewEntity(newId)
+			fmt.Println("Initialized entity: ", newEntity)
 		case c := <-srv.unregister:
 			delete(srv.connections, c)
 			close(c.send)
-		case m := <-srv.broadcast:
-			for c := range srv.connections {
-				select {
-				case c.send <- m:
-				default:
-					delete(srv.connections, c)
-					close(c.send)
-				}
-			}
+		case now := <-timer:
+			srv.Update(now)
 		}
 	}
+}
+
+func (srv *CstServer) wsHandler(w http.ResponseWriter, r *http.Request) {
+	ws, err := websocket.Upgrade(w, r, nil, 1024, 1024)
+	if _, ok := err.(websocket.HandshakeError); ok {
+		http.Error(w, "Not a websocket handshake", 400)
+		return
+	} else if err != nil {
+		return
+	}
+	c := &connection{send: make(chan []byte, 256), ws: ws}
+	srv.register <- c
+	defer func() { srv.unregister <- c }()
+	go c.writer()
+	c.reader(srv)
+}
+
+func (srv *CstServer) Update(now time.Time) {
+	println("Updating ")
 }
 
 type connection struct {
@@ -55,7 +138,7 @@ type connection struct {
 	send chan []byte
 }
 
-func (c *connection) reader(srv *cstserver) {
+func (c *connection) reader(srv *CstServer) {
 	for {
 		_, message, err := c.ws.ReadMessage()
 		//n := bytes.Index(message, []byte{0})
@@ -64,7 +147,7 @@ func (c *connection) reader(srv *cstserver) {
 		if err != nil {
 			break
 		}
-		srv.broadcast <- message
+		//srv.broadcast <- message
 	}
 	c.ws.Close()
 }
@@ -91,29 +174,21 @@ func homeHandler(c http.ResponseWriter, req *http.Request, homeTempl *template.T
 	homeTempl.Execute(c, req.Host)
 }
 
-func (srv *cstserver) wsHandler(w http.ResponseWriter, r *http.Request) {
-	ws, err := websocket.Upgrade(w, r, nil, 1024, 1024)
-	if _, ok := err.(websocket.HandshakeError); ok {
-		http.Error(w, "Not a websocket handshake", 400)
-		return
-	} else if err != nil {
-		return
-	}
-	c := &connection{send: make(chan []byte, 256), ws: ws}
-	srv.register <- c
-	defer func() { srv.unregister <- c }()
-	go c.writer()
-	c.reader(srv)
-}
-
 func main() {
+
 	flag.Parse()
 
-	var srv = cstserver{
-		broadcast:   make(chan []byte),
+	var srv = CstServer{
 		register:    make(chan *connection),
 		unregister:  make(chan *connection),
-		connections: make(map[*connection]bool),
+		connections: make(map[*connection]uint32),
+	}
+
+	srv.entityIdGen = IdGenerator(0)
+	srv.world = SubGrid{
+		GridCoord: Coord{0, 0},
+		Grid:      make(map[Coord]uint32),
+		Entities:  make(map[uint32]Entity),
 	}
 	go srv.run()
 
@@ -130,4 +205,6 @@ func main() {
 	if err := http.ListenAndServe(*addr, nil); err != nil {
 		log.Fatal("ListenAndServe:", err)
 	}
+
+	println("boo")
 }
