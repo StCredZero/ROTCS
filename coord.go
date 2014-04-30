dd58082d1c13c76fad9aa5ae008941dadf3981ca
StCredZero <stcredzero@gmail.com>
2014-04-29 17:57:53 -0700

    reformat coord.go

--- coord.go
+++ coord.go
@@ -15,34 +15,6 @@ type Coord struct {
 	y int64
 }
 
-type GridCoord struct {
-	x int64
-	y int64
-}
-
-type LCoord struct {
-	x int
-	y int
-}
-
-func (self LCoord) inBounds() bool {
-	return self.x >= 0 && self.y >= 0 &&
-		self.x < subgrid_width && self.y < subgrid_height
-}
-
-func (self LCoord) inShyBounds() bool {
-	return self.x >= 1 && self.y >= 1 &&
-		self.x < subgrid_width-1 && self.y < subgrid_height-1
-}
-
-func loc2grid(d int64, dimSize int64) int64 {
-	if d >= 0 {
-		return (d / dimSize)
-	} else {
-		return ((d - (dimSize - 1)) / dimSize)
-	}
-}
-
 func (self *Coord) Grid() GridCoord {
 	return GridCoord{
 		x: loc2grid(self.x, subgrid_width),
@@ -80,6 +52,15 @@ func (self *Coord) IndexString() string {
 	return `"` + strconv.FormatInt(self.x, 10) + "," + strconv.FormatInt(self.y, 10) + `"`
 }
 
+type GridCoord struct {
+	x int64
+	y int64
+}
+
+func (self *GridCoord) Corner() Coord {
+	return Coord{self.x * subgrid_width, self.y * subgrid_height}
+}
+
 func (gridCoord *GridCoord) WriteTo(b *bytes.Buffer) (int, error) {
 	xn, xerr := b.Write(big.NewInt(gridCoord.x).Bytes())
 	if xerr != nil {
@@ -87,4 +68,27 @@ func (gridCoord *GridCoord) WriteTo(b *bytes.Buffer) (int, error) {
 	}
 	yn, yerr := b.Write(big.NewInt(gridCoord.y).Bytes())
 	return xn + yn, yerr
+}
+
+type LCoord struct {
+	x int
+	y int
+}
+
+func (self LCoord) inBounds() bool {
+	return self.x >= 0 && self.y >= 0 &&
+		self.x < subgrid_width && self.y < subgrid_height
+}
+
+func (self LCoord) inShyBounds() bool {
+	return self.x >= 1 && self.y >= 1 &&
+		self.x < subgrid_width-1 && self.y < subgrid_height-1
+}
+
+func loc2grid(d int64, dimSize int64) int64 {
+	if d >= 0 {
+		return (d / dimSize)
+	} else {
+		return ((d - (dimSize - 1)) / dimSize)
+	}
 }
