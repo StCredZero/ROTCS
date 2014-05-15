115a55e93c1911535d8a604c493a1f8d5f1dfcf8
StCredZero <stcredzero@gmail.com>
2014-05-14 17:13:05 -0700

    terminal removals

--- static/terminal.js
+++ static/terminal.js
@@ -89,10 +89,9 @@ var Terminal = Terminal || function(containerId) {
   window.requestFileSystem = window.requestFileSystem ||
                              window.webkitRequestFileSystem;
 
-  const VERSION_ = '1.0.0';
+  const VERSION_ = '0.1';
   const CMDS_ = [
-    '3d', 'cat', 'cd', 'cp', 'clear', 'date', 'help', 'install', 'ls', 'mkdir',
-    'mv', 'open', 'pwd', 'rm', 'rmdir', 'theme', 'version', 'who', 'wget'
+    'clear', 'date', 'exit', 'help', 'login', 'say', 'version', 'who'
   ];
   const THEMES_ = ['default', 'cream'];
 
@@ -268,19 +267,6 @@ var Terminal = Terminal || function(containerId) {
       }
 
       switch (cmd) {
-        case 'cat':
-          var fileName = args.join(' ');
-
-          if (!fileName) {
-            output('usage: ' + cmd + ' filename');
-            break;
-          }
-
-          read_(cmd, fileName, function(result) {
-            output('<pre>' + result + '</pre>');
-          });
-
-          break;
         case 'clear':
           clear_(this);
           return;
@@ -288,276 +274,35 @@ var Terminal = Terminal || function(containerId) {
           output((new Date()).toLocaleString());
           break;
         case 'exit':
-          if (timer_ != null) {
-            magicWord_.stop();
-            clearInterval(timer_);
-          }
+          output(cmd + ':  is under construction<br>');
           break;
         case 'help':
           output('<div class="ls-files">' + CMDS_.join('<br>') + '</div>');
           //output('<p>Add files by dragging them from your desktop.</p>');
           break;
-        case 'install':
-          // Check is installed.
-          if (window.chrome && window.chrome.app) {
-            if (!window.chrome.app.isInstalled) {
-              try {
-                chrome.app.install();
-              } catch(e) {
-                alert(e + '\nEnable is about:flags');
-              }
-            } else {
-              output('This app is already installed.');
-            }
-          }
+        case 'login':
+          output(cmd + ':  is under construction<br>');
           break;
-        case 'ls':
-          ls_(function(entries) {
-            if (entries.length) {
-              var html = formatColumns_(entries);
-              util.toArray(entries).forEach(function(entry, i) {
-                html.push(
-                    '<span class="', entry.isDirectory ? 'folder' : 'file',
-                    '">', entry.name, '</span><br>');
-              });
-              html.push('</div>');
-              output(html.join(''));
-            }
-          });
-          break;
-        case 'pwd':
-          output(cwd_.fullPath);
-          break;
-        case 'cd':
-          var dest = args.join(' ') || '/';
-
-          cwd_.getDirectory(dest, {}, function(dirEntry) {
-            cwd_ = dirEntry;
-            output('<div>' + dirEntry.fullPath + '</div>');
-
-            // Tell FSN visualizer that we're cd'ing.
-            if (fsn_) {
-              fsn_.contentWindow.postMessage({cmd: 'cd', data: dest}, location.origin);
-            }
-
-          }, function(e) { invalidOpForEntryType_(e, cmd, dest); });
-
-          break;
-        case 'mkdir':
-          var dashP = false;
-          var index = args.indexOf('-p');
-          if (index != -1) {
-            args.splice(index, 1);
-            dashP = true;
-          }
-
-          if (!args.length) {
-            output('usage: ' + cmd + ' [-p] directory<br>');
-            break;
-          }
-
-          // Create each directory passed as an argument.
-          args.forEach(function(dirName, i) {
-            if (dashP) {
-              var folders = dirName.split('/');
-
-              // Throw out './' or '/' if present on the beginning of our path.
-              if (folders[0] == '.' || folders[0] == '') {
-                folders = folders.slice(1);
-              }
-
-              createDir_(cwd_, folders);
-            } else {
-              cwd_.getDirectory(dirName, {create: true, exclusive: true}, function() {
-                // Tell FSN visualizer that we're mkdir'ing.
-                if (fsn_) {
-                  fsn_.contentWindow.postMessage({cmd: 'mkdir', data: dirName}, location.origin);
-                }
-              }, function(e) { invalidOpForEntryType_(e, cmd, dirName); });
-            }
-          });
-          break;
-        case 'cp':
-        case 'mv':
-          var src = args[0];
-          var dest = args[1];
-
-          if (!src || !dest) {
-            output(['usage: ', cmd, ' source target<br>',
-                   '&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;', cmd,
-                   ' source directory/'].join(''));
-            break;
-          }
-
-          var runAction = function(cmd, srcDirEntry, destDirEntry, opt_newName) {
-            var newName = opt_newName || null;
-            if (cmd == 'mv') {
-                srcDirEntry.moveTo(destDirEntry, newName);
+        case 's':
+        case 'say':
+          if (game_) {
+              if (args.length == 0) {
+                  output('you said nothing<br>');
               } else {
-                srcDirEntry.copyTo(destDirEntry, newName);
+                  game_.sendMessage(args.join(' '));
               }
-          };
-
-          // Moving to a folder? (e.g. second arg ends in '/').
-          if (dest[dest.length - 1] == '/') {
-            cwd_.getDirectory(src, {}, function(srcDirEntry) {
-              // Create blacklist for dirs we can't re-create.
-              var create = [
-                '.', './', '..', '../', '/'].indexOf(dest) != -1 ? false : true;
-
-              cwd_.getDirectory(dest, {create: create}, function(destDirEntry) {
-                runAction(cmd, srcDirEntry, destDirEntry);
-              }, errorHandler_);
-            }, function(e) {
-              // Try the src entry as a file instead.
-              cwd_.getFile(src, {}, function(srcDirEntry) {
-                cwd_.getDirectory(dest, {}, function(destDirEntry) {
-                  runAction(cmd, srcDirEntry, destDirEntry);
-                }, errorHandler_);
-              }, errorHandler_);
-            });
-          } else { // Treat src/destination as files.
-            cwd_.getFile(src, {}, function(srcFileEntry) {
-              srcFileEntry.getParent(function(parentDirEntry) {
-                runAction(cmd, srcFileEntry, parentDirEntry, dest);
-              }, errorHandler_);
-            }, errorHandler_);
-          }
-
-          break;
-        case 'open':
-          var fileName = args.join(' ');
-
-          if (!fileName) {
-            output('usage: ' + cmd + ' filename');
-            break;
-          }
-
-          open_(cmd, fileName, function(fileEntry) {
-            var myWin = window.open(fileEntry.toURL(), 'mywin');
-          });
-
-          break;
-        case 'init':
-          if (worker_) {
-            worker_.postMessage({cmd: 'init', type: type_, size: size_});
-          }
-          break;
-        case 'rm':
-          // Remove recursively? If so, remove the flag(s) from the arg list.
-          var recursive = false;
-          ['-r', '-f', '-rf', '-fr'].forEach(function(arg, i) {
-            var index = args.indexOf(arg);
-            if (index != -1) {
-              args.splice(index, 1);
-              recursive = true;
-            }
-          });
-
-          // Remove each file passed as an argument.
-          args.forEach(function(fileName, i) {
-            cwd_.getFile(fileName, {}, function(fileEntry) {
-              fileEntry.remove(function() {
-                // Tell FSN visualizer that we're rm'ing.
-                if (fsn_) {
-                  fsn_.contentWindow.postMessage({cmd: 'rm', data: fileName}, location.origin);
-                }
-              }, errorHandler_);
-            }, function(e) {
-              if (recursive && e.code == FileError.TYPE_MISMATCH_ERR) {
-                cwd_.getDirectory(fileName, {}, function(dirEntry) {
-                  dirEntry.removeRecursively(null, errorHandler_);
-                }, errorHandler_);
-              } else if (e.code == FileError.INVALID_STATE_ERR) {
-                output(cmd + ': ' + fileName + ': is a directory<br>');
-              } else {
-                errorHandler_(e);
-              }
-            });
-          });
-          break;
-        case 'rmdir':
-          // Remove each directory passed as an argument.
-          args.forEach(function(dirName, i) {
-            cwd_.getDirectory(dirName, {}, function(dirEntry) {
-              dirEntry.remove(function() {
-                // Tell FSN visualizer that we're rmdir'ing.
-                if (fsn_) {
-                  fsn_.contentWindow.postMessage({cmd: 'rm', data: dirName}, location.origin);
-                }
-              }, function(e) {
-                if (e.code == FileError.INVALID_MODIFICATION_ERR) {
-                  output(cmd + ': ' + dirName + ': Directory not empty<br>');
-                } else {
-                  errorHandler_(e);
-                }
-              });
-            }, function(e) { invalidOpForEntryType_(e, cmd, dirName); });
-          });
-          break;
-        case 'sudo':
-          if (timer_ != null) {
-            magicWord_.stop();
-            clearInterval(timer_);
-          }
-          if (!magicWord_) {
-            magicWord_ = new Sound(true);
-            magicWord_.load('magic_word.mp3', false, function() {
-              magicWord_.play();
-            });
-          } else {
-            magicWord_.play();
-          }
-          timer_ = setInterval(function() {
-            output("<div>YOU DIDN'T SAY THE MAGIC WORD!</div>");
-          }, 100);
-          break;
-        case 'theme':
-          var theme = args.join(' ');
-          if (!theme) {
-            output(['usage: ', cmd, ' ' + THEMES_.join(',')].join(''));
-          } else {
-            if (THEMES_.indexOf(theme) != -1) {
-              setTheme_(theme);
-            } else {
-              output('Error - Unrecognized theme used');
-            }
           }
           break;
         case 'version':
         case 'ver':
           output(VERSION_);
           break;
-        case 'wget':
-          var url = args[0];
-          if (!url) {
-            output(['usage: ', cmd, ' missing URL'].join(''));
-            break;
-          } else if (url.search('^http://') == -1) {
-            url = 'http://' + url;
-          }
-          var xhr = new XMLHttpRequest();
-          xhr.onload = function(e) {
-            if (this.status == 200 && this.readyState == 4) {
-              output('<textarea>' + this.response + '</textarea>');
-            } else {
-              output('ERROR: ' + this.status + ' ' + this.statusText);
-            }
-          };
-          xhr.onerror = function(e) {
-            output('ERROR: ' + this.status + ' ' + this.statusText);
-            output('Could not fetch ' + url);
-          };
-          xhr.open('GET', url, true);
-          xhr.send();
-          break;
         case 'who':
-          output(document.title +
-                 ' - By: Eric Bidelman &lt;ericbidelman@chromium.org&gt;');
+          output(cmd + ':  is under construction<br>');
           break;
         default:
-          if (game_) {
-            game_.sendMessage(lineText);
+          if (cmd) {
+            output(cmd + ': command not found');
           }
       };
 
